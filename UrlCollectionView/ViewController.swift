a42106a2170fedb1dd3c8400cf30ee41f0d53fa0
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-08-16 00:19:15 +0600

    Initial Commit

---
+++ UrlCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrlCollectionView
+//
+//  Created by Olzhas Zhakan on 16.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
